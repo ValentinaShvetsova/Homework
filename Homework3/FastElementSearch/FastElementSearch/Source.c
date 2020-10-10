219f8d7155eee56373d84baf553dfa266595831d
ValentinaShvetsova <Valya151002@gmail.com>
2020-10-10 18:52:32 +0300

    Style guide corrected, qsort corrected

--- Homework3/FastElementSearch/FastElementSearch/Source.c
+++ Homework3/FastElementSearch/FastElementSearch/Source.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-bool binSearch(int array[], int size, int element)
+bool binSearch(int array[], const int size, const int element)
 {
 	int left = 0;
 	int right = size - 1;
@@ -25,9 +25,8 @@ bool binSearch(int array[], int size, int element)
 	return false;
 }
 
-void insertionSort(int leftCounter, int rightCounter, int array[])
+void insertionSort(const int leftCounter, const int rightCounter, int array[])
 {
-
 	for (int i = leftCounter; i <= rightCounter; i++)
 	{
 		int newElement = array[i];
@@ -35,16 +34,19 @@ void insertionSort(int leftCounter, int rightCounter, int array[])
 		while (location >= 0 && array[location] > newElement)
 		{
 			array[location + 1] = array[location];
-			location = location - 1;
+			location--;
 		}
 		array[location + 1] = newElement;
 	}
 }
 
-int findBorderIndex(int array[], int borderElement, int leftBorder, int rightBorder) {
+int findBorderIndex(int array[], int borderElement, const int leftBorder, const int rightBorder) 
+{
 	int borderIndex = 0;
-	for (int i = leftBorder; i < rightBorder; ++i) {
-		if (array[i] >= borderElement) {
+	for (int i = leftBorder; i < rightBorder; ++i) 
+	{
+		if (array[i] >= borderElement) 
+		{
 			borderIndex = i;
 			break;
 		}
@@ -52,9 +54,11 @@ int findBorderIndex(int array[], int borderElement, int leftBorder, int rightBor
 	return borderIndex;
 }
 
-void quickSort(int a[], int leftCounter, int rightCounter) {
-	int size = rightCounter - leftCounter + 1;
-	if (size <= 10) {
+void quickSort(int a[], int leftCounter, int rightCounter) 
+{
+	const int size = rightCounter - leftCounter + 1;
+	if (size <= 10) 
+	{
 		insertionSort(leftCounter, rightCounter, a);
 		return;
 	}
@@ -89,24 +93,28 @@ void quickSort(int a[], int leftCounter, int rightCounter) {
 	quickSort(a, abuttingElementIndex, rightBorder);
 }
 
-bool test1() {
+bool testWithElementInArray() 
+{
 	int array[] = { 1, 2, 3, 3, 5, 6, 7, 8, 3, 1, 0, 8, 3, 9, 1 };
 	int size = 15;
 	quickSort(array, 0, size - 1);
 	return binSearch(array, size, 5);
 }
 
-bool test2() {
+bool testWithoutElementInArray() 
+{
 	int array[] = { 1, 2, 3, 3, 5, 6, 7, 8, 3, 1, 0, 8, 3, 9, 1 };
 	int size = 15;
 	quickSort(array, 0, size - 1);
 	return binSearch(array, size, 12);
 }
 
-int main() {
-	if (test1() == false || test2() == true) {
+int main() 
+{
+	if (!testWithElementInArray() || testWithoutElementInArray())
+	{
 		printf("Test failed");
-		return 0;
+		return 1;
 	}
 	printf("Input n: ");
 	int n = 0;
@@ -119,7 +127,8 @@ int main() {
 	int *array = (int*)malloc(n * sizeof(int));
 	printf("Final array: ");
 
-	for (int i = 0; i < n; i++) {
+	for (int i = 0; i < n; i++) 
+	{
 		array[i] = rand() % 100;
 		printf("%d ", array[i]);
 	}
@@ -129,7 +138,8 @@ int main() {
 	int *numbers = (int*)malloc(k * sizeof(int));
 	printf("Numbers: ");
 
-	for (int i = 0; i < k; i++) {
+	for (int i = 0; i < k; i++) 
+	{
 		numbers[i] = rand() % 100;
 		printf("%d ", numbers[i]);
 	}
@@ -137,7 +147,7 @@ int main() {
 	printf("\nNumbers found in array: ");
 	for (int i = 0; i < k; i++)
 	{
-		if (binSearch(array, n, numbers[i]) == 1)
+		if (binSearch(array, n, numbers[i]))
 		{
 			printf("%d ", numbers[i]);
 		}
