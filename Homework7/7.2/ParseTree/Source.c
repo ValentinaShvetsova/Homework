b074225e88d7d8a9a635a58d69e9c6176abd43f2
ValentinaShvetsova <Valya151002@gmail.com>
2020-11-13 23:02:33 +0300

    Tests added

--- Homework7/7.2/ParseTree/Source.c
+++ Homework7/7.2/ParseTree/Source.c
@@ -4,7 +4,29 @@
 #include <stdbool.h>
 #include "Tree.h"
 
+bool tests() {
+	char string1[] = "(*(+3 7)9)";
+	char string2[] = "(/(+3 7)5)";
+	int answer1 = 90;
+	int answer2 = 2;
+
+	struct Tree* tree1 = buildTree(string1);
+	struct Tree* tree2 = buildTree(string2);
+
+	int result1 = calculate(tree1);
+	int result2 = calculate(tree2);
+
+	deleteTree(tree1);
+	deleteTree(tree2);
+
+	return answer1 == result1 && answer2 == result2;
+}
+
 int main() {
+	if (!tests) {
+		printf("Tests failed\n");
+		return 1;
+	}
 	FILE* file = fopen("data.txt", "r");
 	if (!file)
 	{
