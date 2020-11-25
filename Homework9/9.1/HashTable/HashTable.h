eb5aeb589232968fc83edf10ab0b828d42f62ae3
ValentinaShvetsova <Valya151002@gmail.com>
2020-11-25 15:24:13 +0300

    h file fulled

--- Homework9/9.1/HashTable/HashTable.h
+++ Homework9/9.1/HashTable/HashTable.h
@@ -1,1 +1,22 @@
 #pragma once
+#include <stdbool.h>
+
+struct HashTable;
+
+// This function creates table
+struct HashTable* createHashTable();
+
+// This function adds value to the table
+void addValueToTable(struct HashTable* table, char* value);
+
+// This function checks if value in the table or not
+bool tableContainsValue(struct HashTable* table, char* value);
+
+// This function prints table
+void printTable(struct HashTable* table);
+
+// This function gets maximum list length
+int maximumListLength(struct HashTable* table);
+
+// This function counts average list length
+float averageListLength(struct HashTable* table);
