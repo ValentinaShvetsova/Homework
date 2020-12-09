e6bb7f5f3b83829f39fffc557e7816119f6ae244
ValentinaShvetsova <Valya151002@gmail.com>
2020-12-09 19:16:09 +0300

    some changes added

--- Homework10/RoadsAndTowns/Source.c
+++ Homework10/RoadsAndTowns/Source.c
@@ -5,78 +5,6 @@
 #include <string.h>
 #include <limits.h>
 
-bool tests() {
-	FILE* file = fopen("test.txt", "r");
-	if (!file)
-	{
-		printf("File not found\n");
-		return 1;
-	}
-
-	int size = 0;
-	int numberOfRoads = 0;
-	fscanf(file, "%d %d", &size, &numberOfRoads);
-
-	int** graph = (int*)malloc(size * size * sizeof(int));
-	for (int i = 0; i < size; ++i)
-	{
-		graph[i] = calloc(size, sizeof(int));
-	}
-
-	for (int i = 0; i < numberOfRoads; ++i)
-	{
-		int firstCityNumber = 0;
-		fscanf(file, "%d", &firstCityNumber);
-		int secondCityNumber = 0;
-		fscanf(file, "%d", &secondCityNumber);
-		int length = 0;
-		fscanf(file, "%d", &length);
-		graph[firstCityNumber][secondCityNumber] = length;
-		graph[secondCityNumber][firstCityNumber] = length;
-	}
-
-	int numberOfCapitals = 0;
-	fscanf(file, "%d", &numberOfCapitals);
-
-	int* capitals = (int*)malloc(numberOfCapitals * sizeof(int));
-
-	struct List** towns = (int*)malloc(numberOfCapitals * sizeof(struct List*));
-	for (int i = 0; i < numberOfCapitals; ++i)
-	{
-		int capitalNumber = 0;
-		fscanf(file, "%d", &capitalNumber);
-		capitals[i] = capitalNumber;
-		towns[i] = createList();
-	}
-	fclose(file);
-	distributeCities(graph, size, capitals, numberOfCapitals, towns);
-	for (int i = 0; i < size; ++i)
-	{
-		free(graph[i]);
-	}
-	free(graph);
-
-	free(capitals);
-	
-	struct List* answer1 = createList();
-	addValue(answer1, 0);
-	addValue(answer1, 4);
-	addValue(answer1, 3);
-
-	struct List* answer2 = createList();
-	addValue(answer2, 1);
-	addValue(answer2, 2);
-	
-	bool testPassed = compareLists(answer1, answer2);
-	free(capitals);
-	for (int i = 0; i < numberOfCapitals; ++i)
-	{
-		deleteList(towns[i]);
-	}
-	free(towns);
-	return testPassed;
-}
-
 void useCity(int** graph, int size, int capitalNumber, int* distance, bool* used, struct List* towns, int* numberOfLeftoverCities) {
 	if (*numberOfLeftoverCities == 0) {
 		return;
@@ -137,6 +65,79 @@ void distributeCities(int** graph, int size, int* capitals, int numberOfCapitals
 		free(distance[i]);
 	}
 	free(distance);
+}
+
+bool tests() {
+	FILE* file = fopen("test.txt", "r");
+	if (!file)
+	{
+		printf("File not found\n");
+		return 1;
+	}
+
+	int size = 0;
+	int numberOfRoads = 0;
+	fscanf(file, "%d %d", &size, &numberOfRoads);
+
+	int** graph = (int*)malloc(size * size * sizeof(int));
+	for (int i = 0; i < size; ++i)
+	{
+		graph[i] = calloc(size, sizeof(int));
+	}
+
+	for (int i = 0; i < numberOfRoads; ++i)
+	{
+		int firstCityNumber = 0;
+		fscanf(file, "%d", &firstCityNumber);
+		int secondCityNumber = 0;
+		fscanf(file, "%d", &secondCityNumber);
+		int length = 0;
+		fscanf(file, "%d", &length);
+		graph[firstCityNumber][secondCityNumber] = length;
+		graph[secondCityNumber][firstCityNumber] = length;
+	}
+
+	int numberOfCapitals = 0;
+	fscanf(file, "%d", &numberOfCapitals);
+
+	int* capitals = (int*)malloc(numberOfCapitals * sizeof(int));
+
+	struct List** towns = (int*)malloc(numberOfCapitals * sizeof(struct List*));
+	for (int i = 0; i < numberOfCapitals; ++i)
+	{
+		int capitalNumber = 0;
+		fscanf(file, "%d", &capitalNumber);
+		capitals[i] = capitalNumber;
+		towns[i] = createList();
+	}
+	fclose(file);
+	distributeCities(graph, size, capitals, numberOfCapitals, towns);
+	for (int i = 0; i < size; ++i)
+	{
+		free(graph[i]);
+	}
+	free(graph);
+
+	free(capitals);
+
+	struct List* answer1 = createList();
+	addValue(answer1, 0);
+	addValue(answer1, 4);
+	addValue(answer1, 3);
+
+	struct List* answer2 = createList();
+	addValue(answer2, 1);
+	addValue(answer2, 2);
+
+	bool testPassed = compareLists(answer1, answer2);
+	for (int i = 0; i < numberOfCapitals; ++i)
+	{
+		deleteList(towns[i]);
+	}
+	free(towns);
+	deleteList(answer1);
+	deleteList(answer2);
+	return testPassed;
 }
 
 int main() {
