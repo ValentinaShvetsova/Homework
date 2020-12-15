39647b2ffb1e0a8f81b41752b36e3b90ac0352ca
ValentinaShvetsova <Valya151002@gmail.com>
2020-12-15 12:00:41 +0300

    new solution added

---
+++ test3.3/Test3.3/List.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include "List.h"
+#include <stdlib.h>
+
+struct Node {
+	int value;
+	struct Node* next;
+};
+
+struct List {
+	struct Node* head;
+	struct Node* tail;
+};
+
+struct List* createList() {
+	return calloc(1, sizeof(struct List));
+}
+
+void addValue(int value, struct List* list) {
+	struct Node* newNode = malloc(sizeof(struct Node));
+	if (newNode == NULL) {
+		return;
+	}
+	newNode->value = value;
+	if (!isEmpty(list)) {
+		list->tail->next = newNode;
+		list->tail = list->tail->next;
+		return;
+	}
+	list->head = newNode;
+	list->tail = list->head;
+}
+
+bool isEmpty(struct List* list) {
+	return list->head == NULL;
+}
+
+void deleteValue(struct List* list, int value) {
+	struct Node* current = list->head;
+	struct Node* previous = NULL;
+	while (current != NULL && current->value != value) {
+		previous = current;
+		current = current->next;
+	}
+	if (current->value != value) {
+		return;
+	}
+	struct Node* temp = current->next;
+	free(current);
+	if (previous == NULL) {
+		list->head = temp;
+	}
+	else {
+		previous->next = temp;
+	}
+}
+
+void deleteHead(struct List* list) {
+	struct Node* current = list->head;
+	struct Node* temp = current->next;
+	free(current);
+	list->head = temp;
+}
+
+void deleteList(struct List* list) {
+	while (!isEmpty(list)) {
+		struct Node* temp = list->head->next;
+		deleteValue(list, list->head->value);
+		list->head = temp;
+	}
+}
+
+void deleteGraph(int** graph, int size) {
+	for (int i = 0; i < size; ++i)
+	{
+		free(graph[i]);
+	}
+	free(graph);
+}
+
+void printList(struct List* list) {
+	struct Node* current = list->head;
+	if (isEmpty(list)) {
+		return;
+	}
+	list->tail->next = NULL;
+	while (current != NULL) {
+		printf("%d ", current->value);
+		current = current->next;
+	}
+}
+
+int returnValue(struct List* list, int positionNumber) {
+	struct Node* current = list->head;
+	for (int i = 1; i < positionNumber; ++i) {
+		current = current->next;
+	}
+	return current->value;
+}
+
+bool compareLists(struct List* list1, struct List* list2) {
+	struct Node* current1 = list1->head;
+	struct Node* current2 = list2->head;
+	while (current1 != NULL && current2 != NULL) {
+		if (current1->value != current2->value) {
+			return false;
+		}
+		current1 = current1->next;
+		current2 = current2->next;
+	}
+	return current1 == NULL && current2 == NULL;
+}
---
+++ test3.3/Test3.3/Source.c
@@ -0,0 +1,58 @@
+#include "List.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+int main() {
+	if (!tests()) {
+		return 1;
+	}
+
+	FILE* file = fopen("input.txt", "r");
+	if (!file)
+	{
+		printf("File not found\n");
+		return 1;
+	}
+
+	int size = 0;
+	int edges = 0;
+	fscanf(file, "%d %d", &size, &edges);
+
+	int** graph = (int*)malloc(size * size * sizeof(int));
+	for (int i = 0; i < size; ++i)
+	{
+		graph[i] = calloc(size, sizeof(int));
+	}
+	for (int i = 0; i < edges; ++i)
+	{
+		int firstVertex = 0;
+		fscanf(file, "%d", &firstVertex);
+		int secondVertex = 0;
+		fscanf(file, "%d", &secondVertex);
+		
+		graph[firstVertex][secondVertex] = 1;
+		graph[secondVertex][firstVertex] = 1;
+	}
+	int* used = calloc(size, sizeof(int));
+	struct List* list = createList();
+	addValue(0, list);
+	while (!isEmpty(list)) {
+		int vertex = returnValue(list, 1);
+		deleteValue(list, vertex);
+		used[vertex] = 2;
+		for (int j = 0; j < size; j++) {
+			if (graph[vertex][j] == 1 && used[j] == 0) {
+				addValue(j, list); 
+				used[j] = 1;
+			}
+		}
+		printf("%d\n", vertex);
+	}
+	deleteGraph(graph, size);
+	free(used);
+	deleteList(list);
+	fclose(file);
+}
