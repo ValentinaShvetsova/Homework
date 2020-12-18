90c82471ddafa8cd14152234f36690a890631376
ValentinaShvetsova <Valya151002@gmail.com>
2020-12-18 17:33:56 +0300

    tests added

--- Homework12/Homework12.1/DigitLexer/Source.c
+++ Homework12/Homework12.1/DigitLexer/Source.c
@@ -3,7 +3,26 @@
 #include <stdbool.h>
 #include "Lexer.h"
 
+bool tests() {
+	char string1[] = "11E1";
+	char string2[] = "2.1E-02";
+	char string3[] = "1E3";
+
+	char string4[] = "12E2E2";
+	char string5[] = "1-3";
+	char string6[] = "-E5.";
+
+	if (!lexer(string1) || !lexer(string2) || !lexer(string3) || lexer(string4) || lexer(string5) || lexer(string6)) {
+		return false;
+	}
+	return true;
+}
+
 int main() {
+	if (!tests()) {
+		printf("Tests didn't pass\n");
+		return 1;
+	}
 	FILE* file = fopen("input.txt", "r");
 	if (!file)
 	{
