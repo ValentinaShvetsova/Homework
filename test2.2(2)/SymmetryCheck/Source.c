0970b07a642507b3b833c1a24ca243c362aa47f2
ValentinaShvetsova <Valya151002@gmail.com>
2020-12-22 14:08:09 +0300

    tests added

--- test2.2(2)/SymmetryCheck/Source.c
+++ test2.2(2)/SymmetryCheck/Source.c
@@ -3,7 +3,55 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+bool test1()
+{
+	struct List* list = createList();
+	addValue(list, 15);
+	addValue(list, 78);
+	addValue(list, 78);
+	addValue(list, 15);
+	const bool testPassed = isSymmetric(list);
+	deleteList(list);
+	return testPassed;
+}
+
+bool test2()
+{
+	struct List* list = createList();
+	addValue(list, 6);
+	addValue(list, 7);
+	addValue(list, 8);
+	addValue(list, 7);
+	addValue(list, 6);
+	const bool testPassed = isSymmetric(list);
+	deleteList(list);
+	return testPassed;
+}
+
+bool test3()
+{
+	struct List* list = createList();
+	addValue(list, 1);
+	addValue(list, 2);
+	addValue(list, 3);
+	addValue(list, 4);
+	addValue(list, 2);
+	addValue(list, 1);
+	const bool testPassed = !isSymmetric(list);
+	deleteList(list);
+	return testPassed;
+}
+
+bool tests()
+{
+	return test1() && test2() && test3();
+}
+
 int main() {
+	if (!tests()) {
+		return -1;
+	}
+
 	FILE* file = fopen("input.txt", "r");
 	if (!file)
 	{
