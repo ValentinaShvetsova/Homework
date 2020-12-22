75ea0d4fdecc1c6592a08a90ce756d9cca9a860f
ValentinaShvetsova <Valya151002@gmail.com>
2020-12-22 20:10:29 +0300

    some changes added

--- Homework11/BoyerMooreHorspoolAlgorithm/Source.c
+++ Homework11/BoyerMooreHorspoolAlgorithm/Source.c
@@ -5,24 +5,26 @@
 
 int searchingSubstring(char* string, char* substring) {
 	int offsetTable[256] = { 0 };
+	const int stringLength = strlen(string);
+	const int substringLength = strlen(substring);
 	for (int i = 0; i < 256; ++i) {
 		offsetTable[i] = strlen(substring);
 	}
-	for (int i = strlen(substring)-2; i >= 0; i--) {
-		if (offsetTable[substring[i]] == strlen(substring)) {
-			offsetTable[substring[i]] = strlen(substring) - i - 1;
+	for (int i = substringLength - 2; i >= 0; i--) {
+		if (offsetTable[substring[i]] == substringLength) {
+			offsetTable[substring[i]] =substringLength - i - 1;
 		}
 	}
-	for (int stringPosition = 0; stringPosition <= strlen(string) - strlen(substring); ++stringPosition) {
-		int substringPosition = strlen(substring) - 1;
+	for (int stringPosition = 0; stringPosition <= stringLength - substringLength; ++stringPosition) {
+		int substringPosition = substringLength - 1;
 		while (substring[substringPosition] == string[substringPosition + stringPosition]) {
 			if (substringPosition == 0) {
 				return stringPosition;
 			}
 			substringPosition--;
 		}
-		if (substringPosition != strlen(substring) - 1) {
-			stringPosition += offsetTable[substring[strlen(substring) - 1]] - 1;
+		if (substringPosition != substringLength - 1) {
+			stringPosition += offsetTable[substring[substringLength - 1]] - 1;
 		}
 		else {
 			stringPosition += offsetTable[string[substringPosition + stringPosition]] - 1;
@@ -30,6 +32,7 @@ int searchingSubstring(char* string, char* substring) {
 	}
 	return -1;
 }
+
 int main() {
 	char string[] = {0};
 	char substring[] = {0};
@@ -39,14 +42,15 @@ int main() {
 		return 1;
 	}
 	fscanf(file, "%s", &string);
-	fscanf(file, "%s", &substring);
 	fclose(file);
-	int result = searchingSubstring(string, substring);
+	printf("Input substring: ");
+	scanf("%s", &substring);
+	const int result = searchingSubstring(string, substring);
 	if (result == -1) {
-		printf("String do not consist such substring\n");
+		printf("String does not contain such substring\n");
 	}
 	else {
-		printf("The first substring's enetering is at %d\n", result);
+		printf("The first substring's enetry is at %d\n", result);
 	}
 	return 0;
 }
