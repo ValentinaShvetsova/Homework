d05cfbb788a148969a044cd90cff581d1d61348f
ValentinaShvetsova <Valya151002@gmail.com>
2021-02-18 17:38:42 +0300

    reverse added

--- SecondSemester/Homework1/BWT/Program.cs
+++ SecondSemester/Homework1/BWT/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace BWT
 {
@@ -42,10 +43,9 @@ namespace BWT
                 }
             }
         }
-        public static char[] BWT(string line)
+        public static char[] BWT(string line, char[,] transpositionTable)
         {
             int size = line.Length + 1;
-            char[,] transpositionTable = new char[size, size];
             var result = new char[size];
             for(int i = 0; i < size - 1; ++i)
             {
@@ -68,14 +68,88 @@ namespace BWT
             return result;
         }
 
+        public static string GetAlphabetString(char[] line)
+        {
+            int alphabetSymbols = line.Distinct().Count();
+            string result = "";
+            for (int i = 0; i < line.Length; i++)
+            {
+                if (!result.Contains(line[i]))
+                {
+                    result += line[i];
+                }
+            }
+            return result;
+        }
+
+        public static int[] GetSymbolsCount(char[] line)
+        {
+            string alphabetOfString = GetAlphabetString(line);
+            var countOfSymbols = new int[alphabetOfString.Length];
+            for (int i = 0; i < countOfSymbols.Length; i++)
+            {
+                countOfSymbols[i] = line.Where(x => x == alphabetOfString[i]).Count();
+            }
+            return countOfSymbols;
+        }
+
+        public static char[] ReverseBWT(char[] line, char[,] transpositionTable)
+        {
+            int length = line.Length;
+            int[] countOfSymbols = GetSymbolsCount(line);
+            int sum = 0;
+            for(int i = 0; i < countOfSymbols.Length; i++)
+            {
+                sum = sum + countOfSymbols[i];
+                countOfSymbols[i] = sum - countOfSymbols[i];
+            }
+            string alphabetOfString = GetAlphabetString(line);
+            var temp = new int[line.Length];
+            for (int i = 0; i < line.Length; i++)
+            {
+                for (int j = 0; j < alphabetOfString.Length; j++)
+                {
+                    if (line[i] == alphabetOfString[j])
+                    {
+                        temp[countOfSymbols[j]] = i;
+                        countOfSymbols[j]++;
+                    }
+                }
+            }
+            int numberOfString = 0;
+            for (int i = 0; i < line.Length; i++)
+            {
+                if(transpositionTable[i, line.Length - 1] == '$')
+                {
+                    numberOfString = i;
+                }
+            }
+            int help = temp[numberOfString];
+            var answer = new char[line.Length]; 
+            for(int i = 0; i < line.Length; i++)
+            {
+                answer[i] = line[help];
+                help = temp[help];
+            }
+            return answer;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Enter the string: ");
             string line = Console.ReadLine();
-            var result = BWT(line);
+            int size = line.Length + 1;
+            char[,] transpositionTable = new char[size, size];
+            var result = BWT(line, transpositionTable);
             for (int i = 0; i < line.Length + 1; ++i)
             {
                 Console.Write(result[i]);
+            }
+            char[] res2 = ReverseBWT(result, transpositionTable);
+            Console.WriteLine();
+            for (int i = 0; i < line.Length + 1; ++i)
+            {
+                Console.Write(res2[i]);
             }
         }
     }
