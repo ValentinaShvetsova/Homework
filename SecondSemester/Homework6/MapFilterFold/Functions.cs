fff2f063e7151fdcd6e696159d96a3fc9252b7ba
ValentinaShvetsova <Valya151002@gmail.com>
2021-03-20 20:57:18 +0300

    functions added

---
+++ SecondSemester/Homework6/MapFilterFold/Functions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MapFilterFold
+{
+    class Functions
+    {
+        public static List<int> Map(List<int> list, Func<int, int> func)
+        {
+            var newList = new List<int>();
+            foreach(var element in list)
+            {
+                newList.Add(func(element));
+            }
+            return newList;
+        }
+
+        public static List<int> Filter(List<int> list, Func<int, bool> func)
+        {
+            var newList = new List<int>();
+            foreach (var element in list)
+            {
+                if (func(element))
+                {
+                    newList.Add(element);
+                }
+            }
+            return newList;
+        }
+
+        public static int Fold(List<int> list, int initial, Func<int, int, int> func)
+        {
+            var accumulated = initial;
+            foreach (var elem in list)
+            {
+                accumulated = func(accumulated, elem);
+            }
+            return accumulated;
+        }
+    }
+}
