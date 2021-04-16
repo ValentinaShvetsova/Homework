106294383df52dd0494a6b31e198a9e9a5fbdfe5
ValentinaShvetsova <Valya151002@gmail.com>
2021-04-16 11:42:35 +0300

    files added

---
+++ SecondSemester/Test1/EmtyQueueException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Test1
+{
+    /// <summary>
+    /// Thrown when the queue is empty
+    /// </summary>
+    [Serializable]
+    public class EmtyQueueException : Exception
+    {
+        public EmtyQueueException() { }
+
+        public EmtyQueueException(string message) : base(message) { }
+
+        public EmtyQueueException(string message, Exception inner) : base(message, inner) { }
+
+        protected EmtyQueueException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
---
+++ SecondSemester/Test1/PriorityQueue.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test1
+{
+    public class PriorityQueue<T>
+    {
+        private class Node<T>
+        {
+            public T Value { get; set; }
+            public int Priority { get; set; }
+            public Node<T> Next { get; set; }
+
+            public Node(T value, int priority, Node<T> next)
+            {
+                Value = value;
+                Priority = priority;
+                Next = next;
+            }
+
+        }
+    }
+}
