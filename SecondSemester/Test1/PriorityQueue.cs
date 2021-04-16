b28a13996866e07b1ed17d7454e4fa62d568f7f6
ValentinaShvetsova <Valya151002@gmail.com>
2021-04-16 12:04:56 +0300

    new func added

--- SecondSemester/Test1/PriorityQueue.cs
+++ SecondSemester/Test1/PriorityQueue.cs
@@ -4,21 +4,57 @@ using System.Text;
 
 namespace Test1
 {
-    public class PriorityQueue<T>
+    public class PriorityQueue<TValue>
     {
-        private class Node<T>
-        {
-            public T Value { get; set; }
-            public int Priority { get; set; }
-            public Node<T> Next { get; set; }
+        private int size = 0;
 
-            public Node(T value, int priority, Node<T> next)
+        private Node<TValue> head;
+        private class Node<TValue>
+        {
+            public TValue Value { get; set; }
+            public int Priority { get; set; }
+            public Node<TValue> Next { get; set; }
+
+            /// <summary>
+            /// Initialize new Node in queue
+            /// </summary>
+            /// <param name="value">nodes value</param>
+            /// <param name="priority">nodes priority</param>
+            /// <param name="next">next queue element</param>
+            public Node(TValue value, int priority, Node<TValue> next)
             {
                 Value = value;
                 Priority = priority;
                 Next = next;
             }
+        }
 
+        /// <summary>
+        /// Checks whether the queue is empty or not.
+        /// </summary>
+        public bool IsEmpty
+            => size == 0;
+
+        /// <summary>
+        /// Add new element in the queue based on its priority
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="priority"></param>
+        public void Enqueue(TValue value, int priority)
+        {
+            size++;
+            if (IsEmpty || head.Priority < priority)
+            {
+                head = new Node<TValue>(value, priority, head);
+                return;
+            }
+            var currentElement = head;
+            while(currentElement.Next != null || currentElement.Next.Priority >= priority)
+            {
+                currentElement = currentElement.Next;
+            }
+            var newElement = new Node<TValue>(value, priority, currentElement.Next);
+            currentElement.Next = newElement;
         }
     }
 }
