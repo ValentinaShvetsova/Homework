cd4fbb7f311cf539cc05820ff0b434b90cb5c37b
ValentinaShvetsova <Valya151002@gmail.com>
2021-05-25 21:19:23 +0300

    some tests added

--- SecondSemester/Homework3/BTree.Tests/TreeTests.cs
+++ SecondSemester/Homework3/BTree.Tests/TreeTests.cs
@@ -29,5 +29,58 @@ namespace BTree.Tests
             Assert.AreEqual("b", tree["2"]);
             Assert.AreEqual("f", tree["20"]);
         }
+
+        [Test]
+        public void ExistingTest()
+        {
+            tree.Add("1", "a");
+            tree.Add("2", "b");
+            tree.Add("3", "c");
+
+            tree.Add("9", "d");
+            tree.Add("10", "e");
+            tree.Add("20", "f");
+
+            Assert.AreEqual(true, tree.ContainsKey("2"));
+            Assert.AreEqual(true, tree.ContainsKey("20"));
+            Assert.AreEqual(false, tree.ContainsKey("567"));
+        }
+
+        [Test]
+        public void DeleteTest()
+        {
+            tree.Add("1", "a");
+            tree.Add("2", "b");
+            tree.Add("3", "c");
+
+            tree.Add("9", "d");
+            tree.Add("10", "e");
+            tree.Add("20", "f");
+
+            tree.Remove("3");
+            tree.Remove("10");
+
+            Assert.AreEqual(false, tree.ContainsKey("3"));
+            Assert.AreEqual(false, tree.ContainsKey("10"));
+            Assert.AreEqual(true, tree.ContainsKey("9"));
+        }
+
+        [Test]
+        public void ForEachTest()
+        {
+            tree.Add("1", "a");
+            tree.Add("2", "b");
+            tree.Add("3", "c");
+
+            tree.Add("9", "d");
+            tree.Add("10", "e");
+            tree.Add("20", "f");
+
+            foreach(var data in tree)
+            {
+                Assert.IsNotNull(data.Key);
+                Assert.IsNotNull(data.Value);
+            }
+        }
     }
 }
